620b6f77ca33f5656471eddae86d304a7b9a7453
yjl <yjl_1567@qq.com>
2024-01-08 22:52:25 +0800

    添加颜色

--- include/Color.h
+++ include/Color.h
@@ -25,9 +25,17 @@ namespace csh {
      */
     MAYBE_UNUSED static constexpr Color Black(0, 0, 0);
     /**
+     * 浅红色
+     */
+    MAYBE_UNUSED static constexpr Color LightRed(255, 64,64);
+    /**
      * 红色
      */
     MAYBE_UNUSED static constexpr Color Red(255, 0, 0);
+    /**
+     * 浅绿色
+     */
+     MAYBE_UNUSED static constexpr Color LightGreen(64,255,64);
     /**
      * 绿色
      */
@@ -49,9 +57,17 @@ namespace csh {
      */
     MAYBE_UNUSED static constexpr Color Cyan(0, 255, 255);
     /**
+     * 浅灰色
+     */
+    MAYBE_UNUSED static constexpr Color LightGray(192,192,192);
+    /**
      * 灰色（中间）
      */
     MAYBE_UNUSED static constexpr Color Gray(128, 128, 128);
+    /**
+     * 暗灰色
+     */
+    MAYBE_UNUSED static constexpr Color DarkGray(64,64,64);
 
 }
 
